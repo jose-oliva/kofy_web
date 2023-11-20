91ff6ca8c53858b9c1599f68671c3a879260a5b9
Jose-Oliva-Ing <jose.oliva.ing@gmail.com>
2023-11-19 21:03:23 -0600

    first beta

---
+++ src/pages/Calendario.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
+
+// eslint-disable-next-line react/destructuring-assignment
+const PropertyPane = (props) => <div className="mt-5">{props.children}</div>;
+
+const Scheduler = () => {
+  const [scheduleObj, setScheduleObj] = useState();
+
+  const change = (args) => {
+    scheduleObj.selectedDate = args.value;
+    scheduleObj.dataBind();
+  };
+
+  const onDragStart = (arg) => {
+    // eslint-disable-next-line no-param-reassign
+    arg.navigation.enable = true;
+  };
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Calendario" />
+      <ScheduleComponent
+        height="650px"
+        ref={(schedule) => setScheduleObj(schedule)}
+        selectedDate={new Date(2021, 0, 10)}
+        eventSettings={{ dataSource: scheduleData }}
+        dragStart={onDragStart}
+      >
+        <ViewsDirective>
+          { ['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => <ViewDirective key={item} option={item} />)}
+        </ViewsDirective>
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+      <PropertyPane>
+        <table
+          style={{ width: '100%', background: 'white' }}
+        />
+      </PropertyPane>
+    </div>
+  );
+};
+
+export default Scheduler;
