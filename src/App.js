66e66a805ce94ef5cc1c56c40b912344cdbb0ae8
Jose-Oliva-Ing <jose.oliva.ing@gmail.com>
2023-11-21 06:52:58 -0600

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import LoginPage from './pages/login/login';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Calendario, Empleados, Apilada, Registrar, Kanban, Area, Barras, Pastel } from './pages';
@@ -10,7 +11,13 @@ import './App.css';
 import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
-  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+  const { setCurrentColor, setCurrentMode, currentMode } = useStateContext();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
 
   useEffect(() => {
     const currentThemeColor = localStorage.getItem('colorMode');
@@ -24,71 +31,79 @@ const App = () => {
   return (
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-            <TooltipComponent
-              content="Ajustes"
-              position="TopCenter"
-            >
-              <button
-                type="button"
-                onClick={() => setThemeSettings(true)}
-                style={{ background: currentColor, borderRadius: '50%' }}
-                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
-                aria-label="Settings"
-              >
-                <FiSettings />
-              </button>
-
-            </TooltipComponent>
-          </div>
-          {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-              <Sidebar />
-            </div>
+        <Routes>
+          {!isLoggedIn ? (
+            <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
           ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">
-              <Sidebar />
-            </div>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Ecommerce />} />
+              <Route path="ecommerce" element={<Ecommerce />} />
+              <Route path="empleados" element={<Empleados />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="calendario" element={<Calendario />} />
+              <Route path="kanban" element={<Kanban />} />
+              <Route path="area" element={<Area />} />
+              <Route path="barras" element={<Barras />} />
+              <Route path="pastel" element={<Pastel />} />
+              <Route path="apilada" element={<Apilada />} />
+            </Route>
           )}
-          <div
-            className={
-              activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-            }
-          >
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-              <Navbar />
-            </div>
-            <div>
-              {themeSettings && (<ThemeSettings />)}
-
-              <Routes>
-                {/* dashboard  */}
-                <Route path="/" element={(<Ecommerce />)} />
-                <Route path="/ecommerce" element={(<Ecommerce />)} />
-
-                {/* colaboradores  */}
-                <Route path="/empleados" element={<Empleados />} />
-                <Route path="/registrar" element={<Registrar />} />
-
-                {/* planeacion  */}
-                <Route path="/calendario" element={<Calendario />} />
-                <Route path="/kanban" element={<Kanban />} />
-
-                {/* graficas  */}
-                <Route path="/area" element={<Area />} />
-                <Route path="/barras" element={<Barras />} />
-                <Route path="/pastel" element={<Pastel />} />
-                <Route path="/apilada" element={<Apilada />} />
-
-              </Routes>
-            </div>
-            <Footer />
-          </div>
-        </div>
+        </Routes>
       </BrowserRouter>
+    </div>
+  );
+};
+
+const MainLayout = () => {
+  const { activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
+
+  return (
+    <div className="flex relative dark:bg-main-dark-bg">
+      <div
+        className="fixed right-4 bottom-4"
+        style={{ zIndex: '1000' }}
+      >
+        <TooltipComponent
+          content="Ajustes"
+          position="TopCenter"
+        >
+          <button
+            type="button"
+            onClick={() => setThemeSettings(true)}
+            style={{
+              background: currentColor,
+              borderRadius: '50%',
+            }}
+            className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+          >
+            <FiSettings />
+            <span className="sr-only">Settings</span>
+          </button>
+        </TooltipComponent>
+      </div>
+      {activeMenu ? (
+        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+          <Sidebar />
+        </div>
+      ) : (
+        <div className="w-0 dark:bg-secondary-dark-bg">
+          <Sidebar />
+        </div>
+      )}
+      <div
+        className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
+          activeMenu ? 'md:ml-72' : 'flex-2'
+        }`}
+      >
+        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+          <Navbar />
+        </div>
+        <div>
+          {themeSettings && <ThemeSettings />}
+          <Outlet /> {/* Este componente renderiza las rutas anidadas */}
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 };
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock, faKey, faKitMedical } from '@fortawesome/free-solid-svg-icons';
+import './login.css';
+
+function LoginPage({ onLogin }) {
+  const [isSignUpMode, setIsSignUpMode] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSignUpClick = () => {
+    setIsSignUpMode(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUpMode(false);
+  };
+
+  const handleAdminLogin = (e) => {
+    e.preventDefault(); // Prevent default form submission
+    // Replace this with your real authentication logic
+    if (username === 'admin' && password === 'admin') {
+      onLogin();
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
+  return (
+    <div className={`container ${isSignUpMode ? 'sign-up-mode' : ''}`}>
+      <div className="forms-container">
+        <div className="signin-signup">
+          <form action="#" className="sign-in-form">
+            <h2 className="title">Hola 👋 doctor/a</h2>
+            <p className="social-text">Por favor ingresa el Token de tu paciente</p>
+            <div className="input-field">
+              <FontAwesomeIcon icon={faKitMedical} className="my-auto_mx-auto" />
+              <input type="text" placeholder="Token" />
+            </div>
+            <button type="button" className="btn transparent" onClick={handleSignUpClick}>
+              Iniciar Sesion
+            </button>
+          </form>
+          <form action="#" className="sign-up-form">
+            <h2 className="title">Administrador</h2>
+            {error && <div className="error-message">{error}</div>}
+            <div className="input-field">
+              <FontAwesomeIcon icon={faUser} className="my-auto_mx-auto" />
+              <input
+                type="text"
+                placeholder="Usuario"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="input-field">
+              <FontAwesomeIcon icon={faLock} className="my-auto_mx-auto" />
+              <input
+                type="password"
+                placeholder="Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="button" className="btn transparente" onClick={handleAdminLogin}>
+              Iniciar Sesion
+            </button>
+            <p className="social-text">Inicia sesion con llave fisica</p>
+            <div className="social-media">
+              <button type="button" className="social-icon" aria-label="Physical Key Login">
+                <FontAwesomeIcon icon={faKey} className="my-auto_mx-auto" />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="panels-container">
+        <div className="panel left-panel">
+          <div className="content">
+            <h3>Eres Administrador?</h3>
+            <p>
+              Actualmente estas en el inicio de sesion para un doctor/a
+            </p>
+            <button type="button" className="btn transparent" onClick={handleSignUpClick}>
+              Iniciar Sesion como Administrador
+            </button>
+          </div>
+          <img src="/img/dogLogin1.svg" className="image" alt="" />
+        </div>
+        <div className="panel right-panel">
+          <div className="content">
+            <h3>Eres doctor/a?</h3>
+            <p>
+              Por favor regresa al inicio de sesion por token
+            </p>
+            <button type="button" onClick={handleSignInClick} className="btn transparent" id="sign-in-btn">
+              Regresar
+            </button>
+          </div>
+          <img src="/img/dogLogin.svg" className="image" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
