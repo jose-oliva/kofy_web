6b1f5afbdeb58642b46036ec45fb62d6d673adb6
Jose-Oliva-Ing <jose.oliva.ing@gmail.com>
2023-11-29 11:36:21 -0600

    fifty-two commit

--- src/pages/Tarjeta.jsx
+++ src/pages/Tarjeta.jsx
@@ -112,6 +112,84 @@ const Tarjeta = () => {
             <button type="button" className="bg-red-500 text-white p-2 rounded-md">Eliminar</button>
           </div>
         </div>
+
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-full rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-no-repeat bg-cover bg-center border border-gray-300">
+          <div className="flex justify-around items-center">
+            <div
+              type="icon"
+              style={{ backgroundColor: currentColor }}
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+            >
+              <img src="https://kofy-back.onrender.com/images/inhalador/icon.png" alt="Inhalador Icon" className="w-8 h-8" />
+            </div>
+            <div>
+              <p className="text-2xl"> Inhalador </p>
+            </div>
+          </div>
+          <div className="flex justify-around items-center">
+            <div>
+              <p className="font-bold text-gray-400 flex justify-center"> 4 </p>
+              <p className="text-2xl"> Inhalar lentamente, apretar inhalador una vez y continuar inhalando </p>
+              <img src="https://kofy-back.onrender.com/images/inhalador/inhalador-04.png" alt="Sostener Imagen" className="w-full mt-4 rounded-lg" />
+            </div>
+          </div>
+          <div className="flex justify-around items-center mt-4">
+            <button type="button" className="bg-blue-500 text-white p-2 rounded-md">Editar</button>
+            <button type="button" className="bg-red-500 text-white p-2 rounded-md">Eliminar</button>
+          </div>
+        </div>
+
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-full rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-no-repeat bg-cover bg-center border border-gray-300">
+          <div className="flex justify-around items-center">
+            <div
+              type="icon"
+              style={{ backgroundColor: currentColor }}
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+            >
+              <img src="https://kofy-back.onrender.com/images/inhalador/icon.png" alt="Inhalador Icon" className="w-8 h-8" />
+            </div>
+            <div>
+              <p className="text-2xl"> Inhalador </p>
+            </div>
+          </div>
+          <div className="flex justify-around items-center">
+            <div>
+              <p className="font-bold text-gray-400 flex justify-center"> 4 </p>
+              <p className="text-2xl"> Sacar inhalador y aguantar respiración por 10 segundos, luego exhalar </p>
+              <img src="https://kofy-back.onrender.com/images/inhalador/inhalador-05.png" alt="Sostener Imagen" className="w-full mt-4 rounded-lg" />
+            </div>
+          </div>
+          <div className="flex justify-around items-center mt-4">
+            <button type="button" className="bg-blue-500 text-white p-2 rounded-md">Editar</button>
+            <button type="button" className="bg-red-500 text-white p-2 rounded-md">Eliminar</button>
+          </div>
+        </div>
+
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-full rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-no-repeat bg-cover bg-center border border-gray-300">
+          <div className="flex justify-around items-center">
+            <div
+              type="icon"
+              style={{ backgroundColor: currentColor }}
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+            >
+              <img src="https://kofy-back.onrender.com/images/inhalador/icon.png" alt="Inhalador Icon" className="w-8 h-8" />
+            </div>
+            <div>
+              <p className="text-2xl"> Inhalador </p>
+            </div>
+          </div>
+          <div className="flex justify-around items-center">
+            <div>
+              <p className="font-bold text-gray-400 flex justify-center"> 4 </p>
+              <p className="text-2xl"> Coloca de nuevo la tapa sobre la boquilla y asegurarse de que este cerrada </p>
+              <img src="https://kofy-back.onrender.com/images/inhalador/inhalador-06.png" alt="Sostener Imagen" className="w-full mt-4 rounded-lg" />
+            </div>
+          </div>
+          <div className="flex justify-around items-center mt-4">
+            <button type="button" className="bg-blue-500 text-white p-2 rounded-md">Editar</button>
+            <button type="button" className="bg-red-500 text-white p-2 rounded-md">Eliminar</button>
+          </div>
+        </div>
       </div>
     </div>
   );
