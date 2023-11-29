dcfa83dc422e9d13838bdfe88eb353f5fa114f6f
Jose-Oliva-Ing <jose.oliva.ing@gmail.com>
2023-11-29 08:22:42 -0600

    fifty commit

--- src/pages/Tarjeta.jsx
+++ src/pages/Tarjeta.jsx
@@ -27,7 +27,7 @@ const Tarjeta = () => {
       <Header title="Tarjetas" />
       <div className="flex flex-wrap justify-justify">
         {cardCollections.map((collection) => (
-          <div key={collection.id} className="border border-gray-300 m-3 p-3 rounded-xl">
+          <div key={collection.id} className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-xl w-full lg:w-80 p-8 pt-9 m-3 border border-gray-300">
             <div className="flex justify-around items-center">
               <div
                 type="icon"
@@ -37,15 +37,27 @@ const Tarjeta = () => {
                 <img src={collection.icon} alt={`${collection.name} Icon`} className="w-8 h-8" />
               </div>
               <div>
-                <p className="text-2xl">{collection.name}</p>
+                <p className="text-2xl"> {collection.name} </p>
               </div>
             </div>
             {collection.cards.map((card) => (
-              <div key={card.id} className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-xl mt-4 p-4 border border-gray-300">
+              <div key={card.id} className="flex flex-col justify-between mt-4">
+                <div className="flex justify-around items-center">
+                  <div
+                    type="icon"
+                    style={{ backgroundColor: currentColor }}
+                    className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full p-4"
+                  >
+                    <img src={collection.icon} alt={`${collection.name} Icon`} className="w-8 h-8" />
+                  </div>
+                  <div>
+                    <p className="text-2xl"> {collection.name} </p>
+                  </div>
+                </div>
                 <div className="flex justify-around items-center flex-grow">
                   <div>
-                    <p className="font-bold text-gray-400 flex justify-center">{card.index}</p>
-                    <p className="text-2xl">{card.content}</p>
+                    <p className="font-bold text-gray-400 flex justify-center"> {card.index} </p>
+                    <p className="text-2xl"> {card.content} </p>
                     {card.is_video ? (
                       <iframe width="280" height="240" src={card.video_link} title={`${card.content} Video`} className="object-cover rounded-lg" />
                     ) : (
